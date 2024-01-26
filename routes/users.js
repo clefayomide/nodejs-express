6e1cf75e65b78a1df59fa5f2af3d8ae81e3a8188
victor <victorayomide32@gmail.com>
2024-01-26 21:29:33 +0100

    API: user CRUD
    Added crud fuctionality to the user router handler

--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const uuid = require("uuid");
 
-const users = [
-	{
-		userId: 1,
-		id: 1,
-		jobTitle: "Backend Engineer",
-		retired: false,
-	},
-	{
-		userId: 2,
-		id: 2,
-		jobTitle: "Devops Engineer",
-		retired: true,
-	},
-	{
-		userId: 3,
-		id: 3,
-		jobTitle: "Frontend Engineer",
-		retired: false,
-	},
-	{
-		userId: 4,
-		id: 4,
-		jobTitle: "Cloud Engineer",
-		retired: false,
-	},
-	{
-		userId: 5,
-		id: 5,
-		jobTitle: "Cloud Engineer",
-		retired: true,
-	},
-];
+let users = [];
 
 /* GET users listing. */
 router.get("/", function (req, res) {
@@ -67,6 +37,57 @@ router.get("/find", function (req, res) {
 	res
 		.status(200)
 		.json({ message: "Success", statusCode: 200, data: responseData });
+});
+
+// add new user
+router.post("/", function (req, res) {
+	const reqData = req.body;
+	if (!reqData.jobTitle) {
+		res.status(400).json({ message: "jobTitle is required", statusCode: 400 });
+	} else if (!reqData.hasOwnProperty("retired")) {
+		res
+			.status(400)
+			.json({ message: "retired status is required", statusCode: 400 });
+	} else {
+		const userUuid = uuid.v4();
+		const data = {
+			userId: userUuid,
+			id: userUuid,
+			...reqData,
+			retired: reqData.hasOwnProperty("retired") ? reqData.retired : false,
+		};
+		users.push(data);
+		res.status(200).json({
+			message: "User successfully added",
+			data: data,
+			statusCode: 200,
+		});
+	}
+});
+
+// delete user
+router.delete("/", function (req, res) {
+	const id = req.query.id ?? "";
+
+	if (!id) {
+		res
+			.status(422)
+			.json({ message: "One or more missing paramter", statusCode: 422 });
+	} else {
+		const userExist = users.some(({ userId }) => userId === id);
+
+		if (!userExist) {
+			return res
+				.status(404)
+				.json({ message: "user not found", statusCode: 404 });
+		}
+		users = users.filter(({ userId }) => userId !== id);
+		res.status(200).json({
+			message: "User deleted successfully",
+			statusCode: "200",
+			data: users,
+		});
+	}
 });
 
 module.exports = router;
